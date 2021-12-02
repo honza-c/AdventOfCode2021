6caf0d3ac469bfed1ad99103bc3c915e54254563
Jan Čulík <j.culik@quadient.com>
2021-12-02 11:33:46 +0100

    initial commit, implementation of Day01 and Day02

---
+++ Day01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First puzzle answer is: " + Part1());
+            Console.WriteLine("Second puzzle answer is: " + Part2());
+        }
+
+        private static int Part1()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var counter = 0;
+            var prevValue = int.Parse(input[0]);
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                var actualValue = int.Parse(input[i]);
+
+                if (actualValue > prevValue)
+                {
+                    counter++;
+                }
+
+                prevValue = actualValue;
+            }
+
+            return counter;
+        }
+
+        private static int Part2()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var counter = 0;
+            var prevValue = int.MaxValue;
+
+            for (var i = 0; i < input.Length - 2; i++)
+            {
+                var actualValue = 0;
+
+                for (var j = 0; j < 3; j++)
+                {
+                    actualValue += int.Parse(input[i + j]);
+                }
+
+                if (actualValue > prevValue)
+                {
+                    counter++;
+                }
+
+                prevValue = actualValue;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First puzzle answer is: " + Part1());
+            Console.WriteLine("Second puzzle answer is: " + Part2());
+        }
+
+        private static int Part1()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var horizontalPosition = 0;
+            var depth = 0;
+
+            foreach (var instruction in input)
+            {
+                var command = instruction.Split(" ")[0];
+                var value = int.Parse(instruction.Split(" ")[1]);
+
+                switch (command)
+                {
+                    case "up":
+                        depth -= value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "forward":
+                        horizontalPosition += value;
+                        break;
+                }
+            }
+
+            return horizontalPosition * depth;
+        }
+
+        private static int Part2()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var aim = 0;
+            var horizontalPosition = 0;
+            var depth = 0;
+
+            foreach (var instruction in input)
+            {
+                var command = instruction.Split(" ")[0];
+                var value = int.Parse(instruction.Split(" ")[1]);
+
+                switch (command)
+                {
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "forward":
+                        horizontalPosition += value;
+                        depth += aim * value;
+                        break;
+                }
+            }
+
+            return horizontalPosition * depth;
+        }
+    }
+}
